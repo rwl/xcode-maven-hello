05fec0268166f6ca34edf8767820b16a0eb465fb
rwl <r.w.lincoln@gmail.com>
2013-01-27 16:09:20 +0100

    Removing view and view controller.

--- src/xcode/Hello/AppDelegate.h
+++ src/xcode/Hello/AppDelegate.h
@@ -1,19 +1,9 @@
-//
-//  AppDelegate.h
-//  Hello
-//
-//  Created by Richard Lincoln on 11/01/2013.
-//
-//
-
 #import <UIKit/UIKit.h>
-
-@class ViewController;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
 
-@property (strong, nonatomic) ViewController *viewController;
+@property (strong, nonatomic) UILabel *label;
 
 @end
